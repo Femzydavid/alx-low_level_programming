8de4d9e27fa9fafcb77294ceb127344ab2fab3a3
Oluwafemi Oluwalajiki <femzydavid@gmail.com>
2023-03-02 11:15:35 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,46 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
  */
 void print_number(int n)
 {
-	unsigned int n1;
-	n1 = n;
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
+
+	digit = 0;
 	if (n < 0)
 	{
-		_putchar('_');
-		n1 = -n;
+		_putchar('-');
+		temp = -n;
 	}
-	if (n1 / 10 != 0)
+	else
 	{
-		print_number(n1 / 10);
+		temp = n;
 	}
-	_putchar((n1 % 10) + '0');
+
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
+
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
+	}
 }
