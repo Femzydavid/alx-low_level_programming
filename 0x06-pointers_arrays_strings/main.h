52669c578dece010ef60ea6389fcb63c06c197d6
Oluwafemi Oluwalajiki <femzydavid@gmail.com>
2023-03-02 14:36:22 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -8,3 +11,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+char *_strstr(char *haystack, char *needle);
+void print_number(int n);
+#endif
